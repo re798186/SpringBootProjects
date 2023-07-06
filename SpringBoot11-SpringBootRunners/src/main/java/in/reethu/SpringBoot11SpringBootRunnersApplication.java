fff40201557cf5027a603e664d8b1d72bd5cf3a3
re798186 <reethu.thapasya@gmail.com>
2023-07-06 23:41:36 +0400

    Spring Basics

---
+++ SpringBoot11-SpringBootRunners/src/main/java/in/reethu/SpringBoot11SpringBootRunnersApplication.java
@@ -0,0 +1,13 @@
+package in.reethu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot11SpringBootRunnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot11SpringBootRunnersApplication.class, args);
+	}
+
+}
---
+++ SpringBoot11-SpringBootRunners/src/main/java/in/reethu/runner/EmailServiceRunner.java
@@ -0,0 +1,16 @@
+package in.reethu.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceRunner implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("EmailServiceRunner.run()");
+		System.out.println(args.getOptionNames());
+	}
+
+}
